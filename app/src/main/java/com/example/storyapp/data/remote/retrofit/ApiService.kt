23222af7a9c44712187494c63c7ed3a564aa91e1
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-06-16 10:57:07 +0700

    Add file  ApiConfig, ApiService, and RegisterResponse

---
+++ app/src/main/java/com/example/storyapp/data/remote/retrofit/ApiService.kt
@@ -0,0 +1,19 @@
+package com.example.storyapp.data.remote.retrofit
+
+import com.example.storyapp.data.remote.response.RegisterResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface ApiService {
+
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun register(
+        @Field("username") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ) : RegisterResponse
+
+}
