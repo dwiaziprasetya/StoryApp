d9227cbcb6fb89f900638be723011fb942016611
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-06-18 22:43:54 +0700

    Update ApiConfig, ApiService, and Injection

--- app/src/main/java/com/example/storyapp/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/example/storyapp/data/remote/retrofit/ApiService.kt
@@ -3,16 +3,22 @@ package com.example.storyapp.data.remote.retrofit
 import com.example.storyapp.data.remote.response.FileUploadResponse
 import com.example.storyapp.data.remote.response.LoginResponse
 import com.example.storyapp.data.remote.response.RegisterResponse
+import com.example.storyapp.data.remote.response.StoryResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 import retrofit2.http.Path
 
 interface ApiService {
+
+    @GET("stories")
+    suspend fun getStories() : StoryResponse
 
     @FormUrlEncoded
     @POST("register")
--- app/src/main/java/com/example/storyapp/di/Injection.kt
+++ app/src/main/java/com/example/storyapp/di/Injection.kt
@@ -3,10 +3,15 @@
     import android.content.Context
     import com.example.storyapp.data.remote.retrofit.ApiConfig
     import com.example.storyapp.repository.UserRepository
+    import dataStore
+    import kotlinx.coroutines.flow.first
+    import kotlinx.coroutines.runBlocking
 
     object Injection {
         fun provideRepository(context: Context): UserRepository {
-            val apiService = ApiConfig.getApiService()
+            val pref = SessionPreferences.getInstance(context.dataStore)
+            val user = runBlocking { pref.getSession().first() }
+            val apiService = ApiConfig.getApiService(user.token)
             return UserRepository.getInstance(apiService)
         }
 
