e0d13293a77d80ec6f20b523eadc730e708fb717
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-06-18 08:41:45 +0700

    Finished intent camera x feature

--- app/src/main/java/com/example/storyapp/ui/screen/activity/addstory/AddStoryActivity.kt
+++ app/src/main/java/com/example/storyapp/ui/screen/activity/addstory/AddStoryActivity.kt
@@ -1,29 +1,69 @@
 package com.example.storyapp.ui.screen.activity.addstory
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.storyapp.R
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import com.example.storyapp.databinding.ActivityAddStoryBinding
+import com.example.storyapp.ui.screen.activity.camera.CameraActivity
+import com.example.storyapp.ui.screen.activity.camera.CameraActivity.Companion.CAMERAX_RESULT
 
 class AddStoryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddStoryBinding
     private var currentImageUri: Uri? = null
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Permission request granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Permission request denied", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun allPermissionGranted() =
+        ContextCompat.checkSelfPermission(
+            this,
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnGallery.setOnClickListener { startGallery() }
+        if (!allPermissionGranted()) {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
 
+        binding.btnGallery.setOnClickListener { startGallery() }
+        binding.btnCamera.setOnClickListener {
+            startCamera()
+        }
+    }
+
+    private fun startCamera() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERAX_RESULT) {
+            currentImageUri = it.data?.getStringExtra(CameraActivity.EXTRA_CAMERAX_IMAGE)?.toUri()
+            showImage()
+        }
     }
 
     private fun startGallery(){
@@ -48,5 +88,9 @@ class AddStoryActivity : AppCompatActivity() {
                 scaleType = ImageView.ScaleType.CENTER_CROP
             }
         }
+    }
+
+    companion object {
+        private const val REQUIRED_PERMISSION = android.Manifest.permission.CAMERA
     }
 }
